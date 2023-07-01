e1c3f7c4fc5b541b5d470ff10c1e2b27b70af75f
Ambrosio Souza <ambrosio.souza@maps.com.br>
2023-06-30 22:10:10 -0300

    Continuando primeiro teste

--- hangman/src/main/java/coding/dojo/maps/game/Hangman.java
+++ hangman/src/main/java/coding/dojo/maps/game/Hangman.java
@@ -2,16 +2,19 @@ package coding.dojo.maps.game;
 
 import coding.dojo.maps.datasource.PalavrasDatasource;
 
+import java.util.Random;
+
 public class Hangman {
 
-    private final String
-    private final PalavrasDatasource datasource;
+    private final String palavra;
 
     public Hangman(PalavrasDatasource datasource) {
-        this.datasource = datasource;
+        Random r = new Random();
+        var palavras = datasource.getPalavras();
+        this.palavra = palavras[r.nextInt()];
     }
 
     public Character[] getWordTemplate() {
-        return null;
+        return StringUtils.repeat();
     }
 }
