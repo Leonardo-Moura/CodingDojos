568d7a292bdc4222ce608b1997626d6d5e149981
Ambrosio Souza <ambrosio.souza@maps.com.br>
2023-06-30 21:26:58 -0300

    Iniciando implementação do game

---
+++ hangman/src/test/java/coding/dojo/maps/GameStartupTest.java
@@ -0,0 +1,10 @@
+package coding.dojo.maps;
+
+public class GameStartupTest {
+
+    private static String[] PALAVRAS = new String[]{"Banana","","",""};
+
+    public void initGameTest() {
+
+    }
+}
