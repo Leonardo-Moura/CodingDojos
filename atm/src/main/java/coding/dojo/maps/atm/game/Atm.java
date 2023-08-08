e1cd848a9f6c98ff2021d2c4bb3f31056ee1d2b5
Leonardo Moura <leonardo.moura@maps.com.br>
2023-08-08 20:22:19 -0300

    Continuando atm

--- atm/src/main/java/coding/dojo/maps/atm/game/Atm.java
+++ atm/src/main/java/coding/dojo/maps/atm/game/Atm.java
@@ -2,12 +2,24 @@ package coding.dojo.maps.atm.game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Atm {
+
+    private final List<Integer> valoresPossiveis = List.of(100, 50, 20, 10);
+
     public List<Nota> sacar(Integer valor) {
+        Integer valorRestante = valor;
+
+
+        List<Nota> notas = new ArrayList<>();
+        valoresPossiveis.forEach(valorCorrente -> {
+            int numeroDeNotas = valorRestante / valorCorrente;
+            for ()
+        });
+
 
         int number50 = valor / 50;
-        List<Nota> notas = new ArrayList<>();
         for (int i = 0; i < number50; i++) {
             notas.add(new Nota(50));
         }
