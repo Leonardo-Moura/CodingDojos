12716413675f9e112b48b764fd342d68c1ef1b8c
Ambrosio Souza <ambrosio.souza@maps.com.br>
2023-09-12 20:38:34 -0300

    Inicio

---
+++ cheque/src/test/java/coding/dojo/maps/cheque/game/ChequeTest.java
@@ -0,0 +1,21 @@
+package coding.dojo.maps.cheque.game;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+
+
+public class ChequeTest {
+
+    @Test
+    void retornaUmPara1() {
+        ChequeMachine machine = new ChequeMachine();
+        BigDecimal valorNumerico = BigDecimal.valueOf(1);
+
+        String valorTexto = machine.convert(valorNumerico);
+
+        assertEquals();
+    }
+}
