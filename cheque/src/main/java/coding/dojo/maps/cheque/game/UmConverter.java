f82612fc4f7297858a2f850f8f25f9ff6cbbfbd6
Guilherme <guilherme.pontes0209@gmail.com>
2023-09-12 21:16:26 -0300

    a

---
+++ cheque/src/main/java/coding/dojo/maps/cheque/game/UmConverter.java
@@ -0,0 +1,21 @@
+package coding.dojo.maps.cheque.game;
+
+import java.math.BigDecimal;
+
+public enum NumeroConverter {
+
+    um,
+    dois,
+    tres,
+    QUATRO,
+    CINCO,
+    SEIS,
+    SETE,
+    OITO,
+    NOVE
+
+    public String convert() {
+        return this.name()
+    };
+
+}
