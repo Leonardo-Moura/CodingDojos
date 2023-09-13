dd25c72c2ede2873c03230652adfe2a1c30942e3
Leonardo Moura <leonardo.moura@maps.com.br>
2023-09-12 21:02:17 -0300

    Continuando teste

---
+++ cheque/src/main/java/coding/dojo/maps/cheque/game/Cheque.java
@@ -0,0 +1,19 @@
+package coding.dojo.maps.cheque.game;
+
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class Cheque {
+
+    private final BigDecimal valor;
+
+    public String porExtenso() {
+        String valorTexto = " real";
+        if (BigDecimal.ONE.equals(valor)) {
+            valorTexto = "um" + valorTexto;
+        }
+        return valorTexto;
+    }
+}
--- cheque/src/test/java/coding/dojo/maps/cheque/game/ChequeTest.java
+++ cheque/src/test/java/coding/dojo/maps/cheque/game/ChequeTest.java
@@ -4,6 +4,7 @@ package coding.dojo.maps.cheque.game;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,15 +13,29 @@ public class ChequeTest {
 
     @Test
     void retornaUmPara1() {
-        ChequeMachine machine = new ChequeMachine();
-        BigDecimal valorNumerico = BigDecimal.valueOf(1);
+        Cheque machine = new Cheque(BigDecimal.ONE);
 
-        String valorTexto = machine.convert(valorNumerico);
+        String valorTexto = machine.porExtenso();
 
         assertEquals("um real", valorTexto);
     }
 
+    @Test
     void validaReaisPorExtenso() {
+        Map<BigDecimal, String> valores = Map.of(
+                new BigDecimal("1"), "um real",
+                new BigDecimal("2"), "dois reais",
+                new BigDecimal("3"), "três reais",
+                new BigDecimal("4"), "quatro reais",
+                new BigDecimal("5"), "cinco reais",
+                new BigDecimal("6"), "seis reais",
+                new BigDecimal("7"), "sete reais",
+                new BigDecimal("8"), "oito reais",
+                new BigDecimal("9"), "nove reais"
+        );
 
+        for (Map.Entry<BigDecimal, String> valor : valores.entrySet()) {
+            Cheque cheque = new Cheque(valor.getKey());
+        }
     }
 }
