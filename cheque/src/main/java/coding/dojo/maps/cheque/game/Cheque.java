93c7f30cd6aefd855b4fec4a3c7f76d733dbafab
Guilherme <guilherme.pontes0209@gmail.com>
2024-02-01 19:44:41 -0300

    Converter nove, aplicacao dos converter no cheque

--- cheque/src/main/java/coding/dojo/maps/cheque/game/Cheque.java
+++ cheque/src/main/java/coding/dojo/maps/cheque/game/Cheque.java
@@ -1,6 +1,14 @@
 package coding.dojo.maps.cheque.game;
 
+import coding.dojo.maps.cheque.game.converter.CincoConverter;
+import coding.dojo.maps.cheque.game.converter.DoisConverter;
+import coding.dojo.maps.cheque.game.converter.NoveConverter;
 import coding.dojo.maps.cheque.game.converter.NumeroConverter;
+import coding.dojo.maps.cheque.game.converter.OitoConverter;
+import coding.dojo.maps.cheque.game.converter.QuatroConverter;
+import coding.dojo.maps.cheque.game.converter.SeisConverter;
+import coding.dojo.maps.cheque.game.converter.SeteConverter;
+import coding.dojo.maps.cheque.game.converter.TresConverter;
 import coding.dojo.maps.cheque.game.converter.UmConverter;
 
 import java.math.BigDecimal;
@@ -12,9 +20,36 @@ public class Cheque {
 
     private final List<NumeroConverter> numeroConverters = new ArrayList<>();
 
+    private final String sufixo;
+
     public Cheque(BigDecimal valor) {
+        sufixo
         if (BigDecimal.ONE.equals(valor)) {
             numeroConverters.add(new UmConverter());
+        }
+        if (BigDecimal.valueOf(2).equals(valor)) {
+            numeroConverters.add(new DoisConverter());
+        }
+        if (BigDecimal.valueOf(3).equals(valor)) {
+            numeroConverters.add(new TresConverter());
+        }
+        if (BigDecimal.valueOf(4).equals(valor)) {
+            numeroConverters.add(new QuatroConverter());
+        }
+        if (BigDecimal.valueOf(5).equals(valor)) {
+            numeroConverters.add(new CincoConverter());
+        }
+        if (BigDecimal.valueOf(6).equals(valor)) {
+            numeroConverters.add(new SeisConverter());
+        }
+        if (BigDecimal.valueOf(7).equals(valor)) {
+            numeroConverters.add(new SeteConverter());
+        }
+        if (BigDecimal.valueOf(8).equals(valor)) {
+            numeroConverters.add(new OitoConverter());
+        }
+        if (BigDecimal.valueOf(9).equals(valor)) {
+            numeroConverters.add(new NoveConverter());
         }
     }
 
---
+++ cheque/src/main/java/coding/dojo/maps/cheque/game/converter/NoveConverter.java
@@ -0,0 +1,9 @@
+package coding.dojo.maps.cheque.game.converter;
+
+public class NoveConverter implements NumeroConverter {
+
+    @Override
+    public String porExtenso() {
+        return "nove";
+    }
+}
